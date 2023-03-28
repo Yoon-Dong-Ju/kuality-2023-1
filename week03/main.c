e9235741808c07d822979243e24d55ed582078d0
DongJu Yoon <poet0246@korea.ac.kr>
2023-03-28 17:20:33 +0900

    Initial commit

--- week03/main.c
+++ week03/main.c
@@ -1,5 +1,26 @@
+#pragma warning(disable:4996)
+#pragma warning(disable:6031)
 #include <stdio.h>
-int main()
-{
-	printf("Hello world 2222!\n");
+
+int main() {
+	int n1, n2, n3;
+	char c;
+	printf("Input number: ");
+	scanf(" %d %d %d", &n1, &n2, &n3);
+	printf("Input operator: ");
+	scanf(" %c", &c);
+	switch (c) {
+	case '*':
+		printf("n1 * n2 * n3 = %d \n", n1 * n2 * n3);
+		break;
+	case '+':
+		printf("n1 + n2 + n3 = %d \n", n1 + n2 + n3);
+		break;
+	case '-':
+		printf("n1 - n2 - n3 = %d \n", n1 - n2 - n3);
+		break;
+	case '/':
+		printf("n1 / n2 / n3 = %d \n", n1 / n2 / n3);
+		break;
+	}
 }
